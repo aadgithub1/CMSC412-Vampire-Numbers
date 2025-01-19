4c98d7ec696ba96c9dfb7eaaed4db93d4dbc32ac
aadgithub1 <152732836+aadgithub1@users.noreply.github.com>
2025-01-19 14:17:08 -0500

    Finish single thread model.

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         //vampires: 102510, 105210, 105264
         //non-vampires: 100052, 101259, 104125
-
         int i;
         for(i = 1000; i < 10000; i++){
             int divisor;
             for(divisor = 2; divisor < i; divisor++){
+                boolean isVampire = false;
                 if (i % divisor == 0) {
                     int result = i / divisor;
                     String iStr = Integer.toString(i);
@@ -19,21 +22,29 @@ public class Main {
                     if (divStr.length() == resStr.length()) {
                         String[] divCharArray = divStr.split("");
                         String[] resCharArray = resStr.split("");
+                        ArrayList<String> iStrArrayList = new ArrayList<>(Arrays.asList(iStr.split("")));
                         for(int k = 0; k < divStr.length(); k++){
-                            if(!iStr.contains(divCharArray[k]) || !iStr.contains(resCharArray[k])){
-                                //iStr = 5775
-                                //divCharArray = ["7", "5"]
-                                //resCharArry = ["7", "7"]
-                                //we want to not strike a number when it is used
+                            if(iStrArrayList.contains(divCharArray[k])){
+                                iStrArrayList.remove(divCharArray[k]);
+                                if(iStrArrayList.contains(resCharArray[k])){
+                                    iStrArrayList.remove(resCharArray[k]);
+                                } else{
+                                    break;
+                                }
+                            } else{
                                 break;
                             }
 
                             if(k == divStr.length()-1){
                                 System.out.println(i + " w/ fangs: " + divStr + " & " + resStr);
+                                isVampire = true;
                             }
                         }
                     } else{
                         continue;
+                    }
+                    if(isVampire){
+                        break;
                     }
                 } else{
                     continue;

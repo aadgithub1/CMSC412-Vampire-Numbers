d251a64390a825838d12d083b60f9e35c762f34a
aadgithub1 <152732836+aadgithub1@users.noreply.github.com>
2025-01-19 10:31:19 -0500

    Finish pseudocode. Add test file.

--- Main.java
+++ Main.java
@@ -1,6 +1,30 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //vampires: 102510, 105210, 105264
+        //non-vampires: 100052, 101259, 104125
+
+        // int i;
+        //for(i = 100_000; i < 1_000_000; i++){
+            //divisor
+            //for divisor = 2; divisor < i; divisor++
+                //if i % divisor == 0
+                    //result = i / divisor
+                    //String divStr, resStr, iStr
+                    //set them to the vars
+                    //if divStr.len == resStr.len
+                        //for each index position (k) from 0 to < len divStr
+                            //if !iStr.contains(divStr[k]) || !iStr.contains(resStr[k])
+                                //print not a vampire number
+                                //break
+
+                            //if k len divstr - 1
+                                //print i is a vampire number
+
+                    //else continue
+                //else continue
+
+        // }
+        
     }
 }
---
+++ arena.java
@@ -0,0 +1,7 @@
+public class arena {
+    public static void main(String[] args) {
+        String greet = "Hello!";
+
+        System.out.println("greeting not contains 'oll': " + !greet.contains("oll"));
+    }
+}
